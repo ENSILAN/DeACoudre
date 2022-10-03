9f04d01694c680844145e82254d6fbabc22d6fb1
Nathan Fallet <contact@nathanfallet.me>
2022-10-03 20:33:09 +0200

    Fixes in gameplay

--- src/main/java/me/nathanfallet/ensilan/deacoudre/utils/Game.java
+++ src/main/java/me/nathanfallet/ensilan/deacoudre/utils/Game.java
@@ -74,7 +74,7 @@ public class Game extends AbstractGame {
 			state = GameState.FINISHED;
 			Player p = Bukkit.getPlayer(getPlayers().get(0));
 			if (p != null) {
-				Bukkit.broadcastMessage("§e" + p.getName() + "§7 a gagné la partie !");
+				Bukkit.broadcastMessage("§e" + p.getName() + "§7 a gagné la partie de Dé à coudre !");
 				p.getInventory().clear();
 				p.updateInventory();
 				p.setGameMode(GameMode.SPECTATOR);
@@ -112,12 +112,27 @@ public class Game extends AbstractGame {
     // Called every second
 	@Override
     public void mainHandler() {
+		// Check if current jumper is in water to place wool
 		if (currentJumper != null) {
 			Block block = currentJumper.getLocation().getBlock();
+			DACPlayer zp = DeACoudre.getInstance().getPlayer(currentJumper.getUniqueId());
 			if (block.getType().equals(Material.WATER)) {
-				block.setType(makeWool(
-					DeACoudre.getInstance().getPlayer(currentJumper.getUniqueId()).getColor()
-				));
+				// Check for an extra life
+				if (
+					block.getLocation().clone().add(-1, 0, 0).getBlock().getType() != Material.WATER &&
+					block.getLocation().clone().add(1, 0, 0).getBlock().getType() != Material.WATER &&
+					block.getLocation().clone().add(0, 0, -1).getBlock().getType() != Material.WATER &&
+					block.getLocation().clone().add(0, 0, 1).getBlock().getType() != Material.WATER
+				) {
+					zp.setLives(zp.getLives() + 1);
+					for (UUID uuid : getAllPlayers()) {
+						Player p = Bukkit.getPlayer(uuid);
+						p.sendMessage("§e" + currentJumper.getName() + "§7 a fait un dé à coudre, et obtient une vie supplémentaire !");
+					}
+				}
+
+				// Put wool
+				block.setType(makeWool(zp.getColor()));
 				nextJumper();
 			}
 		}
@@ -218,7 +233,7 @@ public class Game extends AbstractGame {
 			DACPlayer zp = DeACoudre.getInstance().getPlayer(uuid);
 			
 			player.teleport(l);
-			player.setGameMode(GameMode.SURVIVAL);
+			player.setGameMode(GameMode.SPECTATOR);
 			zp.setPlaying(true);
 			zp.setLives(1);
 			zp.setColor(color);
@@ -233,9 +248,12 @@ public class Game extends AbstractGame {
 
 	// Go to next jumper
 	public void nextJumper() {
+		// Get players again if needed
 		if (currentTurn == null || !currentTurn.hasNext()) {
 			currentTurn = getPlayers().iterator();
 		}
+
+		// Handle current jumper
 		if (currentJumper != null) {
 			DACPlayer zp = DeACoudre.getInstance().getPlayer(currentJumper.getUniqueId());
 			if (zp.getLives() == 0) {
@@ -249,10 +267,39 @@ public class Game extends AbstractGame {
 			currentJumper.setGameMode(GameMode.SPECTATOR);
 			currentJumper.teleport(getHub());
 		}
-		currentJumper = Bukkit.getPlayer(currentTurn.next());
+
+		// Get next player
+		DACPlayer next = null;
+		do {
+			currentJumper = Bukkit.getPlayer(currentTurn.next());
+			next = DeACoudre.getInstance().getPlayer(currentJumper.getUniqueId());
+		} while (!next.isPlaying());
+		
+		// Make it play
 		currentJumper.teleport(getHub());
 		currentJumper.setGameMode(GameMode.SURVIVAL);
 		currentJumper.sendMessage("§aC'est à ton tour !");
+
+		// Reset arena if filled
+		if (isArenaFilled()) {
+			resetArena();
+		}
+	}
+
+	// Check if arena is full
+	public boolean isArenaFilled() {
+		for (int x = 3; x < 13; x++) {
+			for (int z = 6; z < 15; z++) {
+				if (
+					Bukkit.getWorld("DeACoudre")
+						.getBlockAt((gameNumber - 1) * 32 + x, 1, z)
+						.getType() == Material.WATER
+				) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
     // Reset arena
